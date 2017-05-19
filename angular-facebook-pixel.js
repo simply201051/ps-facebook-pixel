71582ab04e11d82c62a555eac4377d24bbe49518
passer <simply201051@gmail.com>
2017-05-19 14:55:19 +0800

    beta 0.1

--- angular-facebook-pixel.js
+++ angular-facebook-pixel.js
@@ -2,7 +2,7 @@
 	'use strict';
 	angular.module('ps-facebook-pixel', [])
 		.provider('$pixel', function() {
-			var account,
+			var id,
 				currency = 'TWD';
 			var $pixelProvider = {
 				setAccount: setAccount,
@@ -12,27 +12,12 @@
 			return $pixelProvider;
 
 			function init() {
-				! function(f, b, e, v, n, t, s) {
-					if (f.fbq) return;
-					n = f.fbq = function() {
-						n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-					};
-					if (!f._fbq) f._fbq = n;
-					n.push = n;
-					n.loaded = !0;
-					n.version = '2.0';
-					n.queue = [];
-					t = b.createElement(e);
-					t.async = !0;
-					t.src = v;
-					s = b.getElementsByTagName(e)[0];
-					s.parentNode.insertBefore(t, s)
-				}(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
-				fbq('init', account);
+				!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
+				fbq('init', id);
 			}
 
-			function setAccount(val) {
-				account = val;
+			function setId(val) {
+				id = val;
 			}
 
 			function setCurrency(val) {
